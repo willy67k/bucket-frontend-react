dfe153071d78cb123e145bf7fabf604f674a9b95
willy67k <willy67k001@gmail.com>
2025-10-28 01:37:06 +0800

    fix: sui provider config

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 const { networkConfig } = createNetworkConfig({
   testnet: { url: "https://fullnode.testnet.sui.io:443" },
+  mainnet: { url: "https://fullnode.mainnet.sui.io:443" },
 });
 
 createRoot(document.getElementById("root")!).render(
